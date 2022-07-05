81fe6b427c9d51ac808cd3c6dc4f8b641669835b
SibghatUllah <95136091+SibghatUllah26@users.noreply.github.com>
2022-07-05 12:28:18 +0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+var i=0;
+var j=0;
+var k=0;
 document.querySelector(".btn").addEventListener("click",function(){
   var audio = new Audio("media/audio.mp3");
   audio.play();
@@ -7,10 +9,16 @@ document.querySelector(".btn").addEventListener("click",function(){
     var side = "media/H.png";
     document.querySelector("img").setAttribute("src",side);
     document.querySelector("h1").innerHTML=" It's a Head ✌️"
+    var totalHead = ++j;
+    document.querySelectorAll("h2")[0].innerHTML = "Total Head = " + totalHead;
   }else {
     var side = "media/T.png";
     document.querySelector("img").setAttribute("src",side);
     document.querySelector("h1").innerHTML=" ✌️ It's a Tail "
+    var totalTail = ++k;
+    document.querySelectorAll("h2")[2].innerHTML = "Total Tail = " + totalTail;
   }
+  var count = ++i;
+  document.querySelectorAll("h2")[1].innerHTML = "Total Toss = " + count;
 
 });
